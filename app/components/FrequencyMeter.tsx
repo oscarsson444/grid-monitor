ca55086b911034c697d04613b3377181f25f93b4
oscarsson444 <oscarsson444@gmail.com>
2024-02-11 08:44:37 +0100

    feat: Added frequency meter.

--- app/components/FrequencyMeter.tsx
+++ app/components/FrequencyMeter.tsx
@@ -1,39 +1,47 @@
 "use client";
 import { useEffect, useState } from "react";
 
-export const FrequencyMeter = () => {
-  const [frequency, setFrequency] = useState(50);
+interface FrequencyMeterProps {
+  frequency: number;
+}
+
+export const FrequencyMeter = ({ frequency }: FrequencyMeterProps) => {
+  const [margin, setMargin] = useState(0);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      // Generate a random number between 45 and 55
-      const freqDiff = Math.random() * 2 - 1;
-      setFrequency((prev) => {
-        const newFreq = prev + freqDiff;
-        return newFreq;
-      });
-    }, 1000);
-
-    // Clean up function to clear the interval when the component unmounts
-    return () => clearInterval(intervalId);
-  }, []);
+    const centerDiff = frequency - 50;
+    setMargin(centerDiff * 600);
+  }, [frequency]);
 
   return (
     <div className="flex justify-center items-center flex-col">
-      <p className="text-xl font-semibold">{frequency.toFixed(1)}</p>
-      <div className="grid grid-cols-7 w-[400px] h-[50px] border border-white">
+      <p className="text-xl font-semibold">{frequency.toFixed(2)}</p>
+      <div className="grid grid-cols-7 w-[420px] h-[50px] border border-white">
         <div className="col-span-1 h-[50px] bg-[#f87171] border-r-2 border-white" />
         <div className="col-span-2 h-[50px] bg-[#fcd34d] border-r-2 border-white" />
         <div className="col-span-1 h-[50px] bg-[#84cc16] border-r-2 border-white" />
         <div className="col-span-2 h-[50px] bg-[#fcd34d] border-r-2 border-white" />
         <div className="col-span-1 h-[50px] bg-[#f87171]" />
       </div>
-      <div className={`absolute mt-[29px] h-[51px] w-[3px] bg-white`} />
+      <div
+        style={{ marginLeft: `${margin - 2}px` }}
+        className={`absolute mt-[29px] h-[51px] w-[4px] bg-black`}
+      />
       <div>
-        <div className="absolute -ml-[30.5px] h-[15px] w-[2px] bg-white" />
+        <div className="absolute -ml-[32px] h-[15px] w-[2px] bg-white" />
+        <p className="absolute -ml-[47px] mt-[15px]">49.9</p>
       </div>
       <div>
-        <div className="absolute ml-[26.5px] h-[15px] w-[2px] bg-white" />
+        <div className="absolute ml-[28px] h-[15px] w-[2px] bg-white" />
+        <p className="absolute ml-[17px] mt-[15px]">50.1</p>
+      </div>
+      <div>
+        <div className="absolute -ml-[151.5px] h-[15px] w-[2px] bg-white" />
+        <p className="absolute -ml-[166px] mt-[15px]">49.5</p>
+      </div>
+      <div>
+        <div className="absolute ml-[147.5px] h-[15px] w-[2px] bg-white" />
+        <p className="absolute ml-[134px] mt-[15px]">50.5</p>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
+"use client";
+import { useEffect, useState } from "react";
 import { FrequencyMeter } from "./components/FrequencyMeter";
 
 export default function Home() {
+  const [frequency, setFrequency] = useState(50);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const freqDiff = (Math.random() * 2 - 1) * 0.05;
+      setFrequency((prev) => {
+        const newFreq = prev + freqDiff;
+        return newFreq;
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <main className="flex justify-between px-20 items-center gap-10 h-[100vh] w-full">
       <div className="w-[50%] h-[50%] border border-white rounded-lg">
@@ -11,7 +27,7 @@ export default function Home() {
         </div>
       </div>
       <div className="border border-white rounded-lg flex items-center justify-center w-[50%] h-[50%]">
-        <FrequencyMeter />
+        <FrequencyMeter frequency={frequency} />
       </div>
     </main>
   );
